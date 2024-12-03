ed1fee2c3ef9944091c2a9898485b24e78fa86ee
sbframee <104187401+sbframee@users.noreply.github.com>
2024-12-03 20:43:52 +0530

    update

--- src/Pages/orderPrint.jsx
+++ src/Pages/orderPrint.jsx
@@ -1,136 +1,122 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const OrderPrint = React.forwardRef(({ order, latestDeliveryDate, customerDetails }, ref) => (
-  <>
-     <div
-      id="print-content"
-      ref={ref}
-      style={{
-        width: '170mm',
-        height: '128mm',
-        border: '1px solid black',
-        pageBreakAfter: 'always',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-      }}
-    >
-      <table style={{ width: '100%', border: '1px solid black' }}>
-        <thead>
-          <tr>
-            <th
-              style={{
-                fontWeight: '600',
-                fontSize: 'larger',
-                lineHeight: 0.5,
-                textAlign: 'left',
-                padding: '5px',
-              }}
-            >
-              SANJU SK
-            </th>
-          </tr>
-          <tr>
-            <td style={{ fontWeight: '600', fontSize: 'x-small' }}>
-              In Front of Santoshi Mata Mandir,
-            </td>
-            <td></td>
-            <td>Memo:</td>
-          </tr>
-          <tr>
-            <td style={{ fontWeight: '600', fontSize: 'x-small' }}>
-              Krishnapura Ward, Gondia - 441401
-            </td>
-            <td></td>
-            <td>Order Date: {new Date(order.createdAt).toLocaleDateString()}</td>
-          </tr>
-          <tr>
-            <td style={{ fontWeight: '600', fontSize: 'x-small' }}>
-              Email: skgondia@gmail.com
-            </td>
-            <td></td>
-            <td>Invoice No.: {order.Order_Number}</td>
-          </tr>
-          <tr>
-            <td style={{ fontWeight: '600', fontSize: 'x-small' }}>
-              Phone: 9372 633 633
-            </td>
-            <td></td>
-            <td>Delivery Date: {new Date(latestDeliveryDate).toLocaleDateString()}</td>
-          </tr>
-        </thead>
-      </table>
+  <div
+    ref={ref}
+    style={{
+      width: '170mm',  
+      height: '128mm',
+      border: '1px solid black',
+      pageBreakAfter: 'always',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+    }}
+  >
+    <table style={{ width: '100%', border: '1px solid black' }}>
+      <thead>
+        <tr>
+          <th style={{ fontWeight: '600', fontSize: 'larger', lineHeight: 0.5, textAlign: 'left', padding: '5px' }}>
+            SANJU SK
+          </th>
+        </tr>
+        <tr>
+          <td style={{ fontWeight: '600', fontSize: 'x-small' }}>In Front of Santoshi Mata Mandir,</td>
+          <td></td>
+          <td>Memo:</td>
+        </tr>
+        <tr>
+          <td style={{ fontWeight: '600', fontSize: 'x-small' }}>Krishnapura Ward, Gondia - 441401</td>
+          <td></td>
+          <td>Order Date: {new Date(order.createdAt).toLocaleDateString()}</td>
+        </tr>
+        <tr>
+          <td style={{ fontWeight: '600', fontSize: 'x-small' }}>Email: skgondia@gmail.com</td>
+          <td></td>
+          <td>Invoice No.: {order.Order_Number}</td>
+        </tr>
+        <tr>
+          <td style={{ fontWeight: '600', fontSize: 'x-small' }}>Phone: 9372 633 633</td>
+          <td></td>
+          <td>Delivery Date: {new Date(latestDeliveryDate).toLocaleDateString()}</td>
+        </tr>
+      </thead>
+    </table>
 
-      <table>
-        <tbody>
-          <tr>
-            <td>Party :</td>
-            <td>{customerDetails?.Customer_name}</td>
-            <td>Mobile :</td>
-            <td>{customerDetails?.Mobile_number}</td>
-          </tr>
-        </tbody>
-      </table>
+    <table>
+      <tbody>
+        <tr>
+          <td>Party :</td>
+          <td>{customerDetails?.Customer_name}</td>
+          <td>Mobile :</td>
+          <td>{customerDetails?.Mobile_number}</td>
+        </tr>
+      </tbody>
+    </table>
 
-      <hr />
+    <hr />
 
-      <table>
-        <thead>
-          <tr>
-            <th>Item :</th>
-            <th>Remarks :</th>
-            <th>Qty. :</th>
-            <th>Rate :</th>
-            <th>Amount :</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{order?.Item}</td>
-            <td>{order?.Remark}</td>
-            <td>{order?.Quantity}</td>
-            <td>{order?.Rate}</td>
-            <td>{order?.Amount}</td>
-          </tr>
-        </tbody>
-      </table>
+    <table>
+      <thead>
+        <tr>
+          <th>Item :</th>
+          <th>Remarks :</th>
+          <th>Qty. :</th>
+          <th>Rate :</th>
+          <th>Amount :</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{order?.Item}</td>
+          <td>{order?.Remark}</td>
+          <td>{order?.Quantity}</td>
+          <td>{order?.Rate}</td>
+          <td>{order?.Amount}</td>
+        </tr>
+      </tbody>
+    </table>
 
-      <hr />
+    <hr />
 
-      <table style={{ width: '100%' }}>
-        <thead>
-          <tr style={{ borderBottom: '1px solid #000' }} className="order_item">
-            <td>In Words</td>
-            <td></td>
-            <td></td>
-            <td>Total</td>
-            <td>{order?.Amount}</td>
-          </tr>
-        </thead>
-      </table>
-    </div>
-	<style>
-      {`
-       @media table {
-
-      th, td {
-        padding: 5px;
-        text-align: left;
-        font-size: small;
-      }
-
-      hr {
-        border: 1px solid #000;
-      }
-
-      button, .no-print {
-        display: none !important;
-      }
-    }
-
-      `}
-    </style>
-  </>
+    <table style={{ width: '100%' }}>
+      <thead>
+        <tr style={{ borderBottom: '1px solid #000' }} className="order_item">
+          <td>In Words</td>
+          <td></td>
+          <td></td>
+          <td>Total</td>
+          <td>{order?.Amount}</td>
+        </tr>
+      </thead>
+    </table>
+  </div>
 ));
 
-export default OrderPrint;
+
+const OrderPage = ({ order, latestDeliveryDate, customerDetails }) => {
+  const printRef = useRef();
+
+  const handlePrint = () => {
+    const printWindow = window.open('', '_blank');
+    printWindow.document.write('<html><head><title>Order Print</title></head><body>');
+    printWindow.document.write(printRef.current.outerHTML);
+    printWindow.document.write('</body></html>');
+    printWindow.document.close();
+    printWindow.print();  
+  };
+
+  return (
+    <div>
+      <button onClick={handlePrint}>Print Order</button>
+      <OrderPrint
+        ref={printRef}
+        order={order}
+        latestDeliveryDate={latestDeliveryDate}
+        customerDetails={customerDetails}
+      />
+    </div>
+  );
+};
+
+export default OrderPage;
