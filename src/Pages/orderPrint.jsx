d531474c7e4a7aec1dd29621a3f2a984917b9a7d
sbframee <104187401+sbframee@users.noreply.github.com>
2024-12-03 19:14:40 +0530

    update

--- src/Pages/orderPrint.jsx
+++ src/Pages/orderPrint.jsx
@@ -1,94 +1,135 @@
 import React from 'react';
 
-const OrderPrint = React.forwardRef(({order, latestDeliveryDate, customerDetails}, ref) => (
+const OrderPrint = React.forwardRef(({ order, latestDeliveryDate, customerDetails }, ref) => (
   <>
-    <div ref={ref} style={{
-      width: "170mm",
-      height: "128mm",
-      border: "1px solid black",
-      pageBreakAfter: "always",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "space-between"
-    }}>
-      <table style={{ width: "100%", border: "1px solid black" }}>
-        <tr>
-          <td
-            style={{
-              fontWeight: "600",
-              fontSize: "larger",
-              lineHeight: 0.5
-            }}
-          >
-            SANJU SK
-          </td>
-        </tr>
-        <tr>
-          <td style={{ fontWeight: "600", fontSize: "x-small" }}>
-            In Front of Santoshi Mata Mandir,
-          </td>
-          <td></td>
-          <td>Memo:</td>
-        </tr>
-        <tr>
-          <td style={{ fontWeight: "600", fontSize: "x-small" }}>
-            Krishnapura Ward, Gondia - 441401
-          </td>
-          <td></td>
-          <td>Order Date: {new Date(order.createdAt).toLocaleDateString()}</td>
-        </tr>
-        <tr>
-          <td style={{ fontWeight: "600", fontSize: "x-small" }}>Email: skgondia@gmail.com</td>
-          <td></td>
-          <td>Invoice No.: {order.Order_Number}</td>
-        </tr>
-        <tr>
-          <td style={{ fontWeight: "600", fontSize: "x-small" }}>Phone: 9372 633 633</td>
-          <td></td>
-          <td>Delivery Date: {new Date(latestDeliveryDate).toLocaleDateString()}</td>
-        </tr>
+    <div
+      ref={ref}
+      style={{
+        width: '170mm',
+        height: '128mm',
+        border: '1px solid black',
+        pageBreakAfter: 'always',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+      }}
+    >
+      <table style={{ width: '100%', border: '1px solid black' }}>
+        <thead>
+          <tr>
+            <th
+              style={{
+                fontWeight: '600',
+                fontSize: 'larger',
+                lineHeight: 0.5,
+                textAlign: 'left',
+                padding: '5px',
+              }}
+            >
+              SANJU SK
+            </th>
+          </tr>
+          <tr>
+            <td style={{ fontWeight: '600', fontSize: 'x-small' }}>
+              In Front of Santoshi Mata Mandir,
+            </td>
+            <td></td>
+            <td>Memo:</td>
+          </tr>
+          <tr>
+            <td style={{ fontWeight: '600', fontSize: 'x-small' }}>
+              Krishnapura Ward, Gondia - 441401
+            </td>
+            <td></td>
+            <td>Order Date: {new Date(order.createdAt).toLocaleDateString()}</td>
+          </tr>
+          <tr>
+            <td style={{ fontWeight: '600', fontSize: 'x-small' }}>
+              Email: skgondia@gmail.com
+            </td>
+            <td></td>
+            <td>Invoice No.: {order.Order_Number}</td>
+          </tr>
+          <tr>
+            <td style={{ fontWeight: '600', fontSize: 'x-small' }}>
+              Phone: 9372 633 633
+            </td>
+            <td></td>
+            <td>Delivery Date: {new Date(latestDeliveryDate).toLocaleDateString()}</td>
+          </tr>
+        </thead>
       </table>
+
       <table>
-        <tr>
-          <td>Party :</td>
-          <td>{customerDetails?.Customer_name}</td> 
-          <td>Mobile :</td>
-          <td>{customerDetails?.Mobile_number}</td>
-        </tr>
+        <tbody>
+          <tr>
+            <td>Party :</td>
+            <td>{customerDetails?.Customer_name}</td>
+            <td>Mobile :</td>
+            <td>{customerDetails?.Mobile_number}</td>
+          </tr>
+        </tbody>
       </table>
+
       <hr />
+
       <table>
-        <tr>
-          <td>Item :</td>
-          <td>Remarks :</td>
-          <td>Qty. :</td>
-          <td>Rate :</td>
-          <td>Amount :</td>
-        </tr>
-        <tr>
-          <td>{order?.Item}</td>
-          <td>{order?.Remark}</td>
-          <td>{order?.Quantity}</td>
-          <td>{order?.Rate}</td>
-          <td>{order?.Amount}</td>
-        </tr>
+        <thead>
+          <tr>
+            <th>Item :</th>
+            <th>Remarks :</th>
+            <th>Qty. :</th>
+            <th>Rate :</th>
+            <th>Amount :</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{order?.Item}</td>
+            <td>{order?.Remark}</td>
+            <td>{order?.Quantity}</td>
+            <td>{order?.Rate}</td>
+            <td>{order?.Amount}</td>
+          </tr>
+        </tbody>
       </table>
+
       <hr />
-      <table style={{ width: "100%" }}>
-        <tr style={{ borderBottom: "1px solid #000" }} className="order_item">
-          <td>In Words</td>
-          <td></td>
-          <td></td>
-          <td>Total</td>
-          <td>{order?.Amount}</td> 
-        </tr>
+
+      <table style={{ width: '100%' }}>
+        <thead>
+          <tr style={{ borderBottom: '1px solid #000' }} className="order_item">
+            <td>In Words</td>
+            <td></td>
+            <td></td>
+            <td>Total</td>
+            <td>{order?.Amount}</td>
+          </tr>
+        </thead>
       </table>
     </div>
+
     <style>
       {`
         @media print {
           button {
             display: none !important;
+          }
+
+          /* Ensure that the print layout looks good */
+          table {
+            width: 100%;
+            border-collapse: collapse;
+          }
+
+          th, td {
+            padding: 5px;
+            text-align: left;
+            font-size: small;
+          }
+          
+          hr {
+            border: 1px solid #000;
           }
         }
       `}
