f3c465cd4c1368285a9f7d7949c8decc2a6ceca7
sbframee <104187401+sbframee@users.noreply.github.com>
2024-12-06 16:05:37 +0530

    update

--- src/Pages/orderPrint.jsx
+++ src/Pages/orderPrint.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
-const OrderPrint = React.forwardRef((_, ref) => (
-	<>
-	<div ref={ref}>
-	  <p>Test Content</p>
-	</div>
-	  </>
-  ));
+const OrderPrint = React.forwardRef(({ order, latestDeliveryDate, customerDetails }, ref) => (
+  <div ref={ref} className="print-container">
+    <h1>Order Details</h1>
+    <p>Order Number: {order.Order_Number}</p>
+    <p>Customer Name: {customerDetails?.Customer_name}</p>
+    <p>Contact: {customerDetails?.Mobile_number}</p>
+    <p>Delivery Date: {latestDeliveryDate}</p>
+    <p>Remarks: {order.Remark}</p>
+  </div>
+));
 
 export default OrderPrint;
