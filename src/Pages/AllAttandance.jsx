edfe9e3dfe9781860d4e058c04d5fbfb355aaac2
mesanjusk <mesanjusk@gmail.comm>
2025-05-13 12:41:12 +0530

    final attandace

--- src/Pages/AllAttandance.jsx
+++ src/Pages/AllAttandance.jsx
@@ -201,7 +201,7 @@ export default function AllAttandance() {
 
     return (
         
-             <div className="bg-[#e5ddd5]">
+             <div className="bg-[#e5ddd5] pt-5 max-w-8xl mx-auto px-2">
                 <div className="flex flex-col md:flex-row md:items-end md:justify-between gap-4 p-2">
                     <div className="bg-white overflow-x-auto w-full ">
                         <table className="min-w-full text-sm text-center border">
